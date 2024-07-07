dc72028ea823010ac7d69364fac756adec73faea
luluiwana <lulukiwanatul@gmail.com>
2024-07-07 13:27:55 +0700

    revisi

--- src/views/KDView.vue
+++ src/views/KDView.vue
@@ -1,118 +1,235 @@
 <template>
-    <ion-page>
-        <ion-content :fullscreen="true">
-            <div id="container">
-                <MobileHeader url="/kdipk">
-                </MobileHeader>
-                <div class="center">
-                    <img src="@/theme/img/menu/menu-ipk.png" id="header-icon">
-                    <img src="@/theme/img/header/kd.png" id="header-title">
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <div id="container">
+        <MobileHeader url="/kdipk"> </MobileHeader>
+        <div class="center">
+          <img src="@/theme/img/menu/menu-ipk.png" id="header-icon" />
+          <img src="@/theme/img/header/cp1.png" id="header-title" />
+        </div>
+        <div v-if="currentPage === 'page1'" id="page1">
+          <div class="bg-white">
+            <div class="bg-yellow">
+              <!-- ipk -->
+              <p>
+                Fase F (Umumnya untuk kelas XI dan XII SMA/MA/Program Paket C)
+                <br />
+                <br />
+                Pada akhir fase F, Peserta didik memiliki kemampuan untuk
+                memahami perhitungan kimia, sifat, struktur dan interaksi
+                partikel dalam membentuk berbagai senyawa termasuk pengolahan
+                dan penerapannya dalam keseharian; memahami konsep laju reaksi
+                dan kesetimbangan reaksi kimia; memahami konsep larutan dalam
+                keseharian; memahami konsep termokimia dan elektrokimia;
+                memahami kimia organik termasuk penerapannya dalam keseharian.
+                Konsep-konsep tersebut memungkinkan peserta didik untuk
+                menerapkan dan mengembangkan keterampilan inkuiri sains mereka.
+                <br />
+                <br />
+                Fase F Berdasarkan Elemen
+                <br />
+                <br />
 
-                </div>
-
-                <div class="bg-white">
-                    <div class="bg-yellow">
-                        <!-- ipk -->
-                        <p>
-                            3.10 Menjelaskan konsep asam basa serta kekuatannya dan kesetimbangan pengionan dalam
-                            larutan.
-
-                        </p>
-                    </div>
-                </div>
-                <div class="bg-white">
-                    <div class="bg-yellow">
-                        <!-- ipk -->
-                        <p>
-                            4.10 Menganalisis trayek perubahan pH beberapa indikator yang diekstrak dari bahan alam
-                            melalui percobaan
-
-                        </p>
-                    </div>
-                </div>
-                <MobileFooter />
-
-
+                <span style="color: #3d3f7d">Elemen Pemahaman IPA </span>
+                <br />
+                <br />
+                Peserta didik memiliki kemampuan memahami konsep mol dan
+                stoikiometri dalam menyelesaikan perhitungan kimia; ikatan kimia
+                dalam kaitannya dengan interaksi antar partikel materi dan sifat
+                fisik materi; teori tumbukan antar partikel materi sebagai dasar
+                konsep laju reaksi; kesetimbangan kimia untuk mengamati perilaku
+                reaktan dan produk pada level mikroskopik; korelasi antara pH
+                larutan asam, basa, garam dan larutan penyangga serta
+                penerapannya dalam kehidupan sehari-hari; termokimia; konsep
+                redoks dan sel elektrokimia sebagai implikasi perubahan materi
+                dan energi yang menyertai reaksi kimia serta penerapannya dalam
+                kehidupan sehari hari; senyawa karbon, hidrokarbon dan
+                turunannya beserta pemanfaatannya dalam kehidupan sehari hari.
+              </p>
             </div>
-        </ion-content>
-    </ion-page>
+          </div>
+          <div style="text-align: center">
+            <ion-button
+              @click="showPage('page2')"
+              router-direction="root"
+              fill="clear"
+              class="right"
+            >
+              <img src="@/theme/img/next.png" class="right" alt="" srcset="" />
+            </ion-button>
+          </div>
+        </div>
+        <div v-else id="page2">
+          <div class="bg-white">
+            <div class="bg-yellow">
+              <!-- ipk -->
+              <p>
+                <span style="color: #3d3f7d">Elemen Keterampilan Proses</span>
+                <br />
+                <br />
+                <span style="color: #ff3131">● Mengamati</span>
+                <br />
+                Peserta didik mengamati fenomena ilmiah dan mencatat hasil
+                pengamatannya dengan memperhatikan detail dari objek yang
+                diamati untuk memunculkan pertanyaan yang akan diselidiki.
+                <br />
+                <span style="color: #ff3131"
+                  >● Mempertanyakan dan memprediksi</span
+                >
+                <br />
+                Peserta didik merumuskan pertanyaan ilmiah tentang hubungan
+                antar variabel dan hipotesis yang dapat diselidiki secara ilmiah
+                <br />
+                <span style="color: #ff3131"
+                  >● Merencanakan dan melakukan penyelidikan Peserta didik</span
+                >
+                <br />
+
+                merencanakan dan memilih metode yang sesuai serta mengendalikan
+                variabel berdasarkan referensi untuk mengumpulkan data yang
+                dapat dipercaya. Peserta didik memilih dan menggunakan alat dan
+                bahan, termasuk penggunaan teknologi digital yang sesuai untuk
+                mengumpulkan serta mencatat data secara sistematis dan akurat.
+                <br />
+                <span style="color: #ff3131"
+                  >● Memproses, menganalisis data dan informasi</span
+                >
+                <br />
+                Peserta didik menafsirkan informasi yang didapatkan dengan jujur
+                dan bertanggung jawab. Menggunakan berbagai metode untuk
+                menganalisa pola dan kecenderungan pada data. Mendeskripsikan
+                hubungan antar variabel serta mengidentifikasi inkonsistensi
+                yang terjadi. Menggunakan data dan rujukan untuk menarik
+                kesimpulan yang konsisten dengan hasil penyelidikan.
+                <br />
+                <span style="color: #ff3131">● Mengevaluasi dan refleksi</span>
+                <br />
+
+                Peserta didik mengidentifikasi sumber ketidakpastian dan
+                kemungkinan penjelasan alternatif dalam rangka mengevaluasi
+                kesimpulan, serta menjelaskan cara spesifik untuk meningkatkan
+                kualitas data. Menganalisis aliditas informasi dari sumber
+                primer dan sekunder dan mengevaluasi pendekatan yang digunakan
+                untuk menyelesaikan masalah dalam penyelidikan.
+                <br />
+                <span style="color: #ff3131"
+                  >● Mengomunikasikan hasil hasilgamati</span
+                >
+                <br />
+                Peserta didik mengomunikasikan hasil penyelidikan secara
+                sistematis dan utuh ditunjang dengan argumen ilmiah dan terbuka
+                terhadap pendapat yang lebih relevan.
+              </p>
+            </div>
+          </div>
+          <div style="text-align: center">
+            <ion-button
+              @click="showPage('page1')"
+              router-direction="root"
+              fill="clear"
+              class="right"
+            >
+              <img
+                src="@/theme/img/previous.png"
+                class="right"
+                alt=""
+                srcset=""
+              />
+            </ion-button>
+          </div>
+        </div>
+
+        <MobileFooter />
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 <script lang="ts">
-import { IonContent, IonPage } from '@ionic/vue';
-import { defineComponent, ref } from 'vue';
-import MobileHeader from '@/components/AppHeader.vue';
-import MobileFooter from '@/components/AppFooter.vue';
-
-
+import { IonContent, IonPage } from "@ionic/vue";
+import { defineComponent, ref } from "vue";
+import MobileHeader from "@/components/AppHeader.vue";
+import MobileFooter from "@/components/AppFooter.vue";
+import click from "@/theme/audio/click.mp3";
+const sound_click = new Audio(click);
 
 export default defineComponent({
-    name: 'kd-view',
-    components: {
-        IonContent,
-        IonPage,
-        MobileHeader,
-        MobileFooter,
+  name: "kd-view",
+  components: {
+    IonContent,
+    IonPage,
+    MobileHeader,
+    MobileFooter,
+  },
+  data() {
+    return {
+      currentPage: "page1", // Initial page to show
+    };
+  },
+  methods: {
+    showPage(pageName: string) {
+      sound_click.play();
+      this.currentPage = pageName; // Update current page based on button click
     },
+  },
 });
 </script>
 <style scoped>
 .bg-white {
-    box-sizing: border-box;
-    position: relative;
-    width: 92%;
-    height: 120px;
-    left: 4%;
-    background: #FFFFFF;
-    border: 1px solid #000000;
-    box-shadow: 5px 10px 5px rgb(0 0 0 / 25%);
-    border-radius: 20px;
-    margin: 10px 0px;
+  box-sizing: border-box;
+  position: relative;
+  width: 92%;
+  height: 910px;
+  left: 4%;
+  background: #ffffff;
+  border: 1px solid #000000;
+  box-shadow: 5px 10px 5px rgb(0 0 0 / 25%);
+  border-radius: 20px;
+  margin: 10px 0px;
 }
 
 p {
-    font-size: 15px;
-    margin: auto;
-    padding: 20px;
+  font-size: 15px;
+  margin: auto;
+  padding: 20px;
 }
 
 .bg-yellow {
-    box-sizing: border-box;
-    position: absolute;
-    width: 98%;
-    height: 110px;
-    left: 0%;
-    background: #FFDE59;
-    border: 1px solid #000000;
-    border-radius: 20px;
+  box-sizing: border-box;
+  position: absolute;
+  width: 98%;
+  height: 900px;
+  left: 0%;
+  background: #ffde59;
+  border: 1px solid #000000;
+  border-radius: 20px;
 }
 
 .center {
-    text-align: center;
+  text-align: center;
 }
 
 .center img {
-    width: 90%;
+  width: 90%;
 }
 
 #header-icon {
-    width: 70px;
-    display: block;
-    margin: auto;
+  width: 70px;
+  display: block;
+  margin: auto;
 }
 
 #header-title {
-    display: block;
-    margin: auto;
+  display: block;
+  margin: auto;
 }
 
 .menu {
-    width: 100px;
+  width: 100px;
 }
 
-@media screen and (min-width:768px) {
-    #header-title {
-        width: 400px;
-    }
+@media screen and (min-width: 768px) {
+  #header-title {
+    width: 400px;
+  }
 }
 </style>
-        
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,134 +1,177 @@
 <template>
-    <ion-page>
-        <ion-content :fullscreen="true">
-            <div id="container">
-                <MobileHeader url="/landing">
-                </MobileHeader>
-                <div class="center">
-                    <img src="@/theme/img/menu/menu.png" class="menu">
-                </div>
-                <div class="menu-container">
-                    <div class="icon2">
-                        <ion-button @click="sound" router-link="/kdipk" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <img src="@/theme/img/menu/menu-ipk.png" class="menu">
-
-                        </ion-button>
-                        <ion-button @click="sound" router-link="/kdipk" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <button class="btn">KD & IPK</button>
-                        </ion-button>
-                    </div>
-                    <div class="icon2">
-                        <ion-button @click="sound" router-link="/materi" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <img src="@/theme/img/menu/menu-materi.png" class="menu">
-                        </ion-button>
-                        <ion-button @click="sound" router-link="/materi" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <button class="btn">MATERI</button>
-                        </ion-button>
-                    </div>
-                </div>
-                <div class="menu-container">
-                    <div class="icon2">
-                        <ion-button @click="sound" router-link="/video" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <img src="@/theme/img/menu/menu-video.png" class="menu">
-                        </ion-button>
-                        <ion-button @click="sound" router-link="/video" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <button class="btn">VIDEO</button>
-                        </ion-button>
-                    </div>
-                    <div class="icon2">
-                        <ion-button @click="sound" router-link="/game" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <img src="@/theme/img/menu/menu-games.png" class="menu">
-                        </ion-button>
-                        <ion-button @click="sound" router-link="/game" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <button class="btn">GAMES</button>
-                        </ion-button>
-                    </div>
-                </div>
-                <div class="menu-container">
-                    <div class="icon1">
-                        <ion-button @click="sound" router-link="/profil" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <img src="@/theme/img/menu/menu-profil.png" class="menu">
-                        </ion-button>
-                        <ion-button @click="sound" router-link="/profil" router-direction="root" fill="clear"
-                            class="btn-menu">
-                            <button class="btn">PROFIL</button>
-                        </ion-button>
-                    </div>
-                </div>
-
-            </div>
-            <div class="container">
-                <MobileFooter>
-                </MobileFooter>
-            </div>
-        </ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <div id="container">
+        <MobileHeader url="/landing"> </MobileHeader>
+        <div class="center">
+          <img src="@/theme/img/menu/menu.png" class="menu" />
+        </div>
+        <div class="menu-container">
+          <div class="icon2">
+            <ion-button
+              @click="sound"
+              router-link="/kdipk"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <img src="@/theme/img/menu/menu-ipk.png" class="menu" />
+            </ion-button>
+            <ion-button
+              @click="sound"
+              router-link="/kdipk"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <button class="btn">CP & TP</button>
+            </ion-button>
+          </div>
+          <div class="icon2">
+            <ion-button
+              @click="sound"
+              router-link="/materi"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <img src="@/theme/img/menu/menu-materi.png" class="menu" />
+            </ion-button>
+            <ion-button
+              @click="sound"
+              router-link="/materi"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <button class="btn">MATERI</button>
+            </ion-button>
+          </div>
+        </div>
+        <div class="menu-container">
+          <div class="icon2">
+            <ion-button
+              @click="sound"
+              router-link="/video"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <img src="@/theme/img/menu/menu-video.png" class="menu" />
+            </ion-button>
+            <ion-button
+              @click="sound"
+              router-link="/video"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <button class="btn">VIDEO</button>
+            </ion-button>
+          </div>
+          <div class="icon2">
+            <ion-button
+              @click="sound"
+              router-link="/game"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <img src="@/theme/img/menu/menu-games.png" class="menu" />
+            </ion-button>
+            <ion-button
+              @click="sound"
+              router-link="/game"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <button class="btn">GAMES</button>
+            </ion-button>
+          </div>
+        </div>
+        <div class="menu-container">
+          <div class="icon1">
+            <ion-button
+              @click="sound"
+              router-link="/profil"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <img src="@/theme/img/menu/menu-profil.png" class="menu" />
+            </ion-button>
+            <ion-button
+              @click="sound"
+              router-link="/profil"
+              router-direction="root"
+              fill="clear"
+              class="btn-menu"
+            >
+              <button class="btn">PROFIL</button>
+            </ion-button>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <MobileFooter> </MobileFooter>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 <script lang="ts">
-import { IonContent, IonPage, IonButton } from '@ionic/vue';
-import { defineComponent, ref } from 'vue';
-import MobileHeader from '@/components/AppHeader.vue';
-import MobileFooter from '@/components/AppFooter.vue';
-import click from '@/theme/audio/click.mp3';
+import { IonContent, IonPage, IonButton } from "@ionic/vue";
+import { defineComponent, ref } from "vue";
+import MobileHeader from "@/components/AppHeader.vue";
+import MobileFooter from "@/components/AppFooter.vue";
+import click from "@/theme/audio/click.mp3";
 const sound_click = new Audio(click);
 
 export default defineComponent({
-    name: 'menu-view',
-    components: {
-        IonContent,
-        IonPage,
-        MobileHeader,
-        IonButton,
-        MobileFooter
+  name: "menu-view",
+  components: {
+    IonContent,
+    IonPage,
+    MobileHeader,
+    IonButton,
+    MobileFooter,
+  },
+  methods: {
+    sound: function () {
+      sound_click.play();
     },
-    methods: {
-        sound: function () {
-            sound_click.play();
-        },
-    }
+  },
 });
 </script>
 <style scoped>
 .menu {
-    width: 100px;
-    margin-left: 10px;
+  width: 100px;
+  margin-left: 10px;
 }
 
-
 .center {
-    text-align: center;
-    margin-top: 2rem;
+  text-align: center;
+  margin-top: 2rem;
 }
 
 .menu-container {
-    width: 100%;
-    display: flex;
-    text-align: center;
-    margin-top: 2rem;
+  width: 100%;
+  display: flex;
+  text-align: center;
+  margin-top: 2rem;
 }
 
 .icon2 {
-    width: 50%;
-
+  width: 50%;
 }
 
 .icon1 {
-    width: 100% !important;
-    padding: 0px 50px;
+  width: 100% !important;
+  padding: 0px 50px;
 }
 
 .btn-menu {
-    height: auto;
-    display: block;
+  height: auto;
+  display: block;
 }
 </style>
-    
